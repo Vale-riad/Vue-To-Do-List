1e70e066289fbe1bbca5738d231c55b83efc8429
Vale-riad <valentinariad91@gmail.com>
2022-11-15 16:14:18 +0100

    Aggiunge array

--- main.js
+++ main.js
@@ -3,7 +3,28 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        toDoList: [
+          {
+              task: "Fare la spesa",
+              done: false
+          },
+         
+          {
+            task: "Fare l'esercizio",
+            done: false
+        },
+        {
+          task: "Cucinare",
+          done: true
+      },
+      {
+        task: "Fare sport",
+        done: true
+    },
+  
+        ]
       }
+      
     }
   }).mount('#app')
+  console.log(toDoList);
